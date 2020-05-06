6184416f73fa957face6e36a26dfbc54aeac0e61
Frederik <frederikzt@gmail.com>
2020-05-06 09:02:42 +0200

    Dataset can keep track of index
    Updated the dataset to hold index instead of an unused label

--- data/classification_data.py
+++ data/classification_data.py
@@ -25,7 +25,7 @@ class ClassificationData(BaseDataset):
         path = self.paths[index][0]
         label = self.paths[index][1]
         mesh = Mesh(file=path, opt=self.opt, hold_history=False, export_folder=self.opt.export_folder)
-        meta = {'mesh': mesh, 'label': label}
+        meta = {'mesh': mesh, 'index': index}
         # get edge features
         edge_features = mesh.extract_features()
         edge_features = pad(edge_features, self.opt.ninput_edges)
